d32002e0793dc3fdb0242a09319287d291ff88ef
felipeldn91 <felipeldn91@gmail.com>
2022-10-26 22:57:07 +0100

    Button test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,6 @@ function App() {
         </p>
         <p>
           <Button>Click Here</Button>
-          <Button>Button test </Button>
         </p>
         <a
           className="App-link"
